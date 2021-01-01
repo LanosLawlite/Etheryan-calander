bf3821bf7dce8c4874ada972ee8672d941c4c078
CodeQuebexCode <76848481+CodeQuebexCode@users.noreply.github.com>
2021-01-02 00:54:51 +0530

    we were talking to trippy

---
+++ random1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class random1
+{
+    public static void main()
+    {
+        while(true)
+        {
+        Scanner sc=new Scanner(System.in);
+        int year=0,month=0,day=0;
+        System.out.print("enter Year:");
+        year=sc.nextInt();
+        System.out.print("enter Month:");
+        month=sc.nextInt();
+        System.out.print("enter Day:");
+        day=sc.nextInt();
+        int no_years=0;
+        if(year<2021)
+        {
+            for(no_years=0;no_years<(2021-year);no_years++)
+            {
+            }
+        }
+        System.out.println("no. of years:"+no_years);
+    }}
+}
+ 
