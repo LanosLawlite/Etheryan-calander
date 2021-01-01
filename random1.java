fc58f51176b894f53c882dc2d7f8e9bf88437b0d
CodeQuebexCode <76848481+CodeQuebexCode@users.noreply.github.com>
2021-01-02 01:04:56 +0530

    previous change: no.of yrs
    Just an update

--- random1.java
+++ random1.java
@@ -5,22 +5,28 @@ class random1
     {
         while(true)
         {
-        Scanner sc=new Scanner(System.in);
-        int year=0,month=0,day=0;
-        System.out.print("enter Year:");
-        year=sc.nextInt();
-        System.out.print("enter Month:");
-        month=sc.nextInt();
-        System.out.print("enter Day:");
-        day=sc.nextInt();
-        int no_years=0;
-        if(year<2021)
-        {
-            for(no_years=0;no_years<(2021-year);no_years++)
+            Scanner sc=new Scanner(System.in);
+            int year=0,month=0,day=0;
+            System.out.print("enter Year:");
+            year=sc.nextInt();
+            System.out.print("enter Month:");
+            month=sc.nextInt();
+            System.out.print("enter Day:");
+            day=sc.nextInt();
+            int no_years=0;
+            if(year<2021)
             {
+                for(no_years=0;no_years<(2021-year);no_years++)
+                {
+                }
             }
-        }
-        System.out.println("no. of years:"+no_years);
-    }}
+            else
+            {
+                for(no_years=0;no_years<(year-2021);no_years++)
+                {
+                }
+            }  
+            System.out.println("no. of years:"+no_years);
+        }}
 }
  
